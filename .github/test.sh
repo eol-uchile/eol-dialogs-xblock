0f1e0ec8bada9ac3735364aa06a483fb7ec7ae1c
matsalinas <matsalinas@uchile.cl>
2021-03-09 14:49:05 +0000

    Update tetst koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,14 +2,14 @@
 
 pip install -e /openedx/requirements/eoldialogs
 
-cd /openedx/requirements/eoldialogs/eoldialogs
+cd /openedx/requirements/eoldialogs
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/eoldialogs/eoldialogs
+cd /openedx/requirements/eoldialogs
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eoldialogs/tests.py
 
 rm -rf test_root
